2eaab9558b64b7758ffd7962d822de0afb76b75c
atnio27 <108244664+atnio27@users.noreply.github.com>
2024-12-12 14:56:11 +0100

    Added houselist header and added some tags and styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
-    <h1>House Listings</h1>
+    <div class="house-list-header">
+      <header>
+        <h1>Houses</h1>
+        <button>Create New</button>
+      </header>
+      <footer>
+        <input type="text" placeholder="Search for a house">
+        <button>Size</button>
+      </footer>
+    </div>
     <HouseList />
   </div>
 </template>
@@ -16,9 +25,55 @@ import HouseList from '../components/HouseList.vue'
   padding: 20px;
 }
 
+.house-list-header {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+
+    h1 {
+      margin: 0;
+      font-size: 24px;
+    }
+
+    button {
+      border: none;
+      border-radius: 8px;
+      padding: 10px;
+      margin: 0px;
+      cursor: pointer;
+      font-size: 16px;
+      background-color: orange;
+    }
+  }
+
+  footer {
+    display: flex;
+    justify-content: space-between;
+
+    input {
+      border: none;
+      background-color: rgb(226, 226, 226);
+      /* padding-left: 10px; */
+      /* padding-right: 10px; */
+      padding-top: 8px;
+      padding-bottom: 8px;
+      padding-left: 26px;
+      padding-right: 200px;
+      border-radius: 4px;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+}
+
 h1 {
   text-align: center;
   margin-bottom: 20px;
 }
 </style>
-
