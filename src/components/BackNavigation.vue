0031e45d8a2b92794319c1076433d02fbd7f45a1
Antonio Esclapez <titoway33@gmail.com>
2024-12-26 15:38:34 +0100

    Created backcomponent to be reusable

---
+++ src/components/BackNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="back-navigation">
+    <router-link to="/houses" class="back-link">
+      <img src="@/assets/icons/ic_back_grey@3x.png" alt="Back" class="icon" />
+      <span>Back to overview</span>
+    </router-link>
+  </div>
+</template>
+
+<style>
+.back-navigation {
+  margin-bottom: 20px;
+
+  .back-link {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    color: var(--secondary-text);
+    text-decoration: none;
+    font-weight: 600;
+  }
+}
+</style>
