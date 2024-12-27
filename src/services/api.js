2b77988ff1b4bbf129cd5573d0fa0a6235c396ec
atnio27 <108244664+atnio27@users.noreply.github.com>
2024-12-27 18:15:47 +0100

    Created uploadImageMethod

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const apiClient = axios.create({
   baseURL: 'https://api.intern.d-tt.nl/api',
   headers: {
-    // 'Content-Type': 'application/json',
+    'Content-Type': 'application/json',
     'X-Api-Key': 'wYhXlJIK-gDvHGP3frbaWzsF7T6MeAN_',
   },
 })
@@ -15,7 +15,17 @@ export default {
   getHouseById(id) {
     return apiClient.get(`/houses/${id}`)
   },
-  createHouse(formData) {
-    return apiClient.post('/houses')
+  createHouse(data) {
+    return apiClient.post('/houses', data)
+  },
+  uploadImage(id, image) {
+    return apiClient.post(`/houses/${id}/upload`, image, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+  },
+  deleteHouse(id) {
+    return apiClient.delete(`/houses/${id}`)
   },
 }
